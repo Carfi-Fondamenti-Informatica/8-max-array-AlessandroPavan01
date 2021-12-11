772f02220f98577943fd81dbb2b4b17eb140672b
AlessandroPavan01 <91598666+AlessandroPavan01@users.noreply.github.com>
2021-12-11 19:07:43 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,10 @@
+//
+// Created by Alessandro on 11/12/2021.
+//
+
+#ifndef ESERCITAZIONE8_LIB_H
+#define ESERCITAZIONE8_LIB_H
+float numeroMax(const float numeri[], int dimensione);
+
+#endif //ESERCITAZIONE8_LIB_H
 
