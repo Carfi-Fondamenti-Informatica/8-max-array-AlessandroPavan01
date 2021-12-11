6521ba05072ee504aaa6895ec7c503f022310b7b
AlessandroPavan01 <91598666+AlessandroPavan01@users.noreply.github.com>
2021-12-11 19:09:26 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+#include "lib.h"
+
+using namespace std;
 
 int main(){
-  return 0;
-}
+    int dim;
+    cin >> dim;
+    float numeri [dim];
+    for (int i=0 ; i < dim ; i++){
+        cin >> numeri[i];
+    }
+    numeroMax(numeri, dim);
+
+    cout<< numeroMax(numeri, dim) <<endl;
+
+    return 0;
